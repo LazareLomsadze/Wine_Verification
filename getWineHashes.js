077c8cbbffd2387004b037f1671bb0d1a7288310
BlockHero <lomsadze.lazare2003@gmail.com>
2024-07-14 05:55:12 +0400

    npm i dachirdeba marto

--- getWineHashes.js
+++ getWineHashes.js
@@ -1,5 +1,7 @@
+const QRCode = require('qrcode');
 const { ethers } = require('ethers');
 const { InfuraProvider } = require('@ethersproject/providers');
+require('dotenv').config();
 
 // Contract ABI (Application Binary Interface)
 const abi = [
@@ -58,6 +60,20 @@ const contractAddress = '0xD039115CD115A53C1481Da1800f94cf89612CCF6';
 // Provider (using a public Ethereum RPC endpoint, e.g., Infura)
 const provider = new InfuraProvider('sepolia', '0b2745c0581a43aba9a132e2975cd118');
 
+// Generate QR Code and save as an image
+function generateQR(data, filename) {
+    return QRCode.toFile(filename, data, {
+        color: {
+            dark: '#000',  // Black dots
+            light: '#FFF' // White background
+        }
+    }).then(() => {
+        console.log(`QR Code generated and saved as ${filename}`);
+    }).catch(err => {
+        console.error(`Error generating QR Code for ${data}:`, err);
+    });
+}
+
 async function getWineHashes() {
     // Create a contract instance
     const contract = new ethers.Contract(contractAddress, abi, provider);
@@ -68,6 +84,8 @@ async function getWineHashes() {
         console.log('Wine Hashes:');
         wineHashes.forEach((hash, index) => {
             console.log(`Hash ${index + 1}: ${hash}`);
+            // Generate a QR code for each hash
+            generateQR(hash, `wine_hash_${index + 1}.png`);
         });
     } catch (error) {
         console.error('Error fetching wine hashes:', error);
